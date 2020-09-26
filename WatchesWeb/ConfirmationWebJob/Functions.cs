6d5e72276920744c59e26e60b349dab61f4bbeee
Vinod Kumar <Vinod.Kumar3@rsystems.com>
2020-09-26 22:56:01 +0530

    Initial commit

---
+++ WatchesWeb/ConfirmationWebJob/Functions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace ConfirmationWebJob
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ConfirmStockCheck([QueueTrigger("stockchecks")] string message, [Blob("confirmations/{id}")] out string output)
+        {
+            var timestamp = message.Split()[3];
+            output = $"{timestamp} stock check successfully processed";
+        }
+    }
+}
---
+++ WatchesWeb/StockCheckWebJob/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage;
+using System.Threading;
+
+namespace StockCheckWebJob
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            //var config = new JobHostConfiguration();
+
+            //if (config.IsDevelopment)
+            //{
+            //    config.UseDevelopmentSettings();
+            //}
+
+            //var host = new JobHost(config);
+            //// The following code ensures that the WebJob will be running continuously
+            //host.RunAndBlock();
+
+
+            var queue = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["StorageAccount"].ConnectionString)
+                .CreateCloudQueueClient()
+                .GetQueueReference("stockchecks");
+
+            queue.CreateIfNotExists();
+
+            while (true)
+            {
+                var timestamp = DateTimeOffset.UtcNow.ToString("s");
+
+                var message = new CloudQueueMessage($"Stock check at {timestamp} completed");
+                queue.AddMessage(message);
+
+                Thread.Sleep(TimeSpan.FromSeconds(30));
+            }
+        }
+    }
+}
